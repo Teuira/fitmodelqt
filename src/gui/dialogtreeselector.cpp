75a9e1dae55a880d609b2ade3f73c8b72f0b45c1
Davide C <cdvd87@yahoo.it>
2019-01-28 12:59:31 +0100

    Format conversion ok.

--- src/gui/dialogtreeselector.cpp
+++ src/gui/dialogtreeselector.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 
 #include <QFileDialog>
+#include <QMessageBox>
 
 DialogTreeSelector::DialogTreeSelector(QWidget *parent) :
     QDialog(parent),
@@ -44,9 +45,35 @@ void DialogTreeSelector::on_btnBrowseTree_clicked()
     std::string content((std::istreambuf_iterator<char>(ifs)),
                            (std::istreambuf_iterator<char>()));
     if (content.find("::") == std::string::npos) {
-        warningBox("Incorrect file format!\nMaybe this file has been created with an older version of FitModel.");
+        QMessageBox::StandardButton reply;
+        reply = QMessageBox::question(this, "Wrong format", "Incorrect file format!\nMaybe this file has been created with an older version of FitModel.\nDo You want to try to convert it?",
+                                        QMessageBox::Yes|QMessageBox::No);
+        if (reply == QMessageBox::Yes) {
+            // Creates a converted copy of the file.
+            std::string converted = "";
+            bool isSecond = false;
+            for (size_t i = 0; i < content.length(); i++) {
+                converted += content[i];
+                if (content[i] == ':') {
+                    if (isSecond) {
+                        converted += ":";
+                        isSecond = false;
+                    }
+                    else {
+                        isSecond = true;
+                    }
+                }
+            }
+            // Saves file.
+            std::string convertedOut = fileName.toStdString() + "_converted_wbest";
+            std::ofstream out(convertedOut);
+            out << converted;
+            out.close();
+            infoBox(QString::fromStdString("New file saved to: " + convertedOut));
+        }
         return;
     }
+    // Sets tree.
     ui->leditTreePath->setText(fileName);
 }
 
